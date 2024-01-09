e936fb002f0177c64c1009bb30c954f9413549c4
Mostafa Magdy <eng.mostafa.magdy.lotfy@gmail.com>
2024-01-09 16:10:37 +0200

    Added dynamic entity generator

---
+++ dynamic-entities-service/src/main/java/com/bass/dms/dynamicentities/DynamicEntitiesServiceApplication.java
@@ -0,0 +1,14 @@
+package com.bass.dms.dynamicentities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+public class DynamicEntitiesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicEntitiesServiceApplication.class, args);
+    }
+
+}
---
+++ dynamic-entities-service/src/test/java/com/bass/dms/dynamicentities/DynamicEntitiesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bass.dms.dynamicentities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicEntitiesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
